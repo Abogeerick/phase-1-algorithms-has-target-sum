42cd89f1bdda6211ef9a781fcb9b397ffc930b10
Abogeerick <abogeerick@gmail.com>
2023-06-30 13:31:41 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set(); // Set to store the numbers we have seen
+
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    if (seen.has(complement)) {
+      return true;
+    }
+    seen.add(array[i]);
+  }
+
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +19,22 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
-*/
+/* function hasTargetSum(array, target)
+  seen = empty Set to store the numbers we have seen
+
+  for i = 0 to array.length - 1
+    complement = target - array[i]
+    if seen contains complement
+      return true
+    add array[i] to seen
+
+  return false
 
 /*
   Add written explanation of your solution here
+   The function iterates through each element in the array and checks if the complement (target minus current element) exists in the array. 
+   If a complement is found, it means that a pair of numbers exists in the array that adds up to the target, and the function returns true. 
+   If no such pair is found after checking all elements, the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
